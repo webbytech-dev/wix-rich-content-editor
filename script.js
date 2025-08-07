9d72c9a5ceab9e88a61b7a1b5cde516c83bf9654
Victor Henrique Cardinali Barros <85972147+victorhcb@users.noreply.github.com>
2025-08-07 15:51:39 +0000

    Adiciona ferramenta Header

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const Header = window.Header;
+
 const editor = new EditorJS({
   holder: "editorjs",
+  tools: {
+    header: {
+      class: Header,
+      config: {
+        levels: [1, 2, 3, 4, 5, 6],
+        defaultLevel: 3,
+      },
+    },
+  },
 });
